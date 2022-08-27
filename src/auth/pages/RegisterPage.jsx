5a554832a59b1e8a51b6948c70083549bc3db607
janegro09 <javier.sosa099@gmail.com>
2022-08-27 19:34:00 -0300

    sumamos pantalla de register

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,58 @@
+import { Google } from '@mui/icons-material'
+import { Button, Grid, TextField, Typography, Link } from '@mui/material'
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom';
+
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout tittle='Crear una cuenta'>
+        <form>
+            <Grid container >
+                <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                    <TextField
+                        label="Nombre completo" 
+                        type="text"
+                        placeholder='Tu nombre'
+                        fullWidth
+                        />
+                </Grid>
+
+                <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                    <TextField 
+                        label="correo" 
+                        type="email"
+                        placeholder='correo@google.com'
+                        fullWidth
+                        />
+                </Grid>
+
+                <Grid item xs={ 12 } sx={{ mt: 2 }}> 
+                    <TextField 
+                        label="Contraseña" 
+                        type="password"
+                        placeholder='Contraseña'
+                        fullWidth
+                        />
+                </Grid>
+
+                <Grid container spacing={ 2 } sx={{ mb: 2, mt:1 }}>
+                    <Grid item xs={ 12 }>
+                        <Button variant='contained' fullWidth>
+                            Crear cuenta
+                        </Button>
+                    </Grid>
+                </Grid>
+
+                <Grid container direction='row' justifyContent='end'>
+                    <Link component={RouterLink} color='inherit' to='/auth/login'>
+                        Crear una cuenta
+                    </Link>
+                </Grid>
+
+            </Grid>
+        </form>
+    </AuthLayout>
   )
 }
